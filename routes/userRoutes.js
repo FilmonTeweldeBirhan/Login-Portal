12911c8e52c570d70c37fc3fcc11778186fc855d
FilmonTeweldeBirhan <filmontewelde415@gmail.com>
2023-11-22 02:04:40 -0800

    layed the tiles for routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+module.exports = router;
---
+++ routes/viewRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Welcome" });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup", { title: "Sign up" });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { title: "Login" });
+});
+
+module.exports = router;
