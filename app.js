e9f27e67ae7de46772205a8349fd2aed8520cca9
FilmonTeweldeBirhan <filmontewelde415@gmail.com>
2023-11-22 21:38:06 -0800

    initialized passport & session

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const expressLayouts = require("express-ejs-layouts");
 // The GLOBAL ERROR HANDLER
 const globalErrorHandler = require("./controllers/errorhandler");
 
+// modules
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // routes
 const userRoutes = require("./routes/userRoutes");
 const viewRoutes = require("./routes/viewRoutes");
@@ -19,6 +32,32 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: false }));
+
+// Express's session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Flash Middleware
+app.use(flash());
+
+// Global flash variables
+app.use((req, res, next) => {
+  req.locals.success_msg = req.flash("success_msg");
+  req.locals.error_msg = req.flash("error_msg");
+  req.locals.error = req.flash("error");
+
+  next();
+});
 
 app.use("/", viewRoutes);
 app.use("/api/v1/users", userRoutes);
